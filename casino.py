584cc5137f7830bccd313dd7d416668e3b74ca2b
Данила <k.danila3001@gmail.com>
2021-04-03 01:09:14 +0000

    casino colorama

--- casino.py
+++ casino.py
@@ -1,19 +1,28 @@
 """Все функции для казино в городе"""
 
+from colorama import Fore, Style, init
 
 import hero
 import random
 import information
 
+TOWN_MESSAGE = Style.DIM + Fore.LIGHTYELLOW_EX
+MESSAGE_DIALOGUE = Fore.GREEN
+HELP_MESSAGE = Fore.CYAN + Style.DIM
+
+init(autoreset=True)
+
 
 def dialogue():
     """Функция для выбора миниигры в рулетке, в которую хочет сыграть игрок и запуска её"""
     while True:
-        choice = input("Здраствуйте. Вы находитесь в казино. Выберите варианты игры\n"
-                       "Золотой куш => 1\n"
-                       'Выйти => 2')
+        choice = input(Style.RESET_ALL + MESSAGE_DIALOGUE + 'Здраствуйте. Вы находитесь в казино.'
+                       ' Выберите варианты игры\n'
+                       'Золотой куш => 1\n'
+                       'Выйти => 2\n')
         if choice == '1':
-            print('Вы ставите золото, с вас его снимают, вам выпадает число золота, которое вы получаете'
+            print(HELP_MESSAGE + 'Вы указываете золото, с вас его снимают, вам выпадает число золота, которое вы'
+                  ' получаете\n'
                   'Число может выпасть в интервале от [0, 2n], где n ваше число')
             start_game(gold_jackpot)
             break
@@ -25,8 +34,7 @@ def start_game(game):
     """Запускает игру заданную в рулетке, то есть даёт полный интерфейс, взаимодёствее перед игрой
     Передаётся: функция игры, в которую игрок хочет играть"""
     while True:
-        what = input("Играть => 1\n"
-                     "Нет => 2\n")
+        what = input(HELP_MESSAGE + 'Играть => 1\n' + 'Нет => 2\n')
         if what == '1':
             game()
         elif what == '2':
@@ -39,17 +47,18 @@ def gold_jackpot():
     из списка [0, его золото*2]"""
     # вводится количество золота, которое игрок ставит
     while True:
-        gold = input("Приветствуем вас в игре золотой куш.\n"
-                     "Введите количество золота, которое вы ставите\n"
-                     "Введите '0', если хотите уйти")
+        gold = input(Style.RESET_ALL + MESSAGE_DIALOGUE + 'Приветствуем вас в игре Золотой куш.\n'
+                     'Введите количество золота, которое вы ставите\n'
+                     'Введите "0", если хотите уйти\n')
         if gold.isdigit() and gold != '0':
             if hero.parameter['gold'] >= int(gold):
                 gold = int(gold)
                 hero.gold_spending(gold)
-                print('Игра начинается\n'
+                print(HELP_MESSAGE + 'Игра начинается\n'
                       'Итак выпало...')
                 information.pause()
                 win_gold = random.randint(0, gold * 2)
+                print(Fore.BLACK + '*' + str(win_gold//2) + '*')
                 hero.gold_receive(win_gold)
                 information.pause()
             else:
